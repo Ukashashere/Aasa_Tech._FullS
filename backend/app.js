4a48d41d4a2c70a9af8c91388a681f9583e7eccb
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-12-16 23:58:19 +0530

    Swagger Integration 1

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const weatherRoutes = require('./routes/weather');
 const reportRoutes = require('./routes/report');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 
 const app = express();
 
@@ -22,9 +25,32 @@ app.get('/', (req, res) => {
     res.send('Backend is running!');
 });
 
-
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// Swagger configuration
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Weather App API',
+            version: '1.0.0',
+            description: 'API documentation for the Weather App',
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // Path to route files where Swagger annotations are added
+};
+
+// Generate Swagger docs
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/* Abhi mera Swagger UI will be available at http://localhost:5000/docs */
