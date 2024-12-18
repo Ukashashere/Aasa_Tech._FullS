388b9cdaf0ffc51a1c0cee1523560e2ed5d25b38
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-12-18 14:45:11 +0530

    Frontend files fix 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,25 @@
+import './App.scss';
+import Home from './Components/Home';     /*Import, Here we have imported things*/
+import Footer from './Components/Footer';
+import Book_Appointment from './Components/Book_Appointment';
+import HowItWorks from './Components/HowItWorks';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Book_Appointment />
+      <Home />         {/*Render, Here we have rendered the imported things*/}
+      <HowItWorks />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+/*
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
@@ -19,3 +41,4 @@ function App() {
 }
 
 export default App;
+*/
